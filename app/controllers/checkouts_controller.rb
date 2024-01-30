5598a2124e7da2e54f411af31ddd7810cb364e4b
German Renato Cobian <germancobian@hotmail.com>
2024-01-30 14:44:35 -0800

    Creates Checkout controller, routes and view for create action + Checkout action in javascript/controllers/cart_controller

---
+++ app/controllers/checkouts_controller.rb
@@ -0,0 +1,6 @@
+class CheckoutsController < ApplicationController
+
+  def create
+    puts "hits create"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
   resources :products, only: [:show]
   get "cart" => "carts#show"
+  post "checkout" => "checkouts#create"
 end
